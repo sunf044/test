298d2036782378feed8041556aff5d1a0bf2fa10
sunf044 <longhai044207@gmail.com>
2021-11-12 01:07:51 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -9,47 +9,4 @@ class Login extends CI_Controller
 		$this->load->model('Admin_model', 'admin');
 	}
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->model('Admin_model', 'admin');
-	}
-
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->model('Admin_model', 'admin');
-	}
-
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->model('Admin_model', 'admin');
-	}
-
-
-	
-	public function tae555()
-	{
-		tae555
-	}
-	
-	public function tae()
-	{
-		parent::tae();
-		$this->load->tae('Admin_model', 'admin');
-	}
-
-	public function tae2()
-	{
-		parent::tae();
-		$this->load->tae('Admin_model', 'admin');
-	}
-	
-	public function tae3()
-	{
-		parent::tae3();
-		$this->load->tae('Admin_model', 'admin');
-	}
-
 }
